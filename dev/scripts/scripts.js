33bd7d9807b3244af4d0f0361a08eceade768192
Jamie Lockie <jamielockie@gmail.com>
2017-10-18 11:28:57 -0400

    appending to page

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -2,42 +2,61 @@ const thisApp = {};
 thisApp.endpoint = 'https://api.github.com/search/issues';
 thisApp.userAgent = 'jamielockie'; 
 thisApp.token = 'c10b97525d3cb61ee37390069c16765fd75f05';
+thisApp.language = 'javascript';
+thisApp.label__primary = "beginner";
+
+
 
 thisApp.init = () => {
-	console.log("connected!");
-	thisApp.getRepos();
+	thisApp.getIssues()
+		.then(thisApp.displayIssues);
 };
 
-thisApp.getRepos = function() {
+thisApp.displayIssues = (issues, i) => {
+	issues.forEach((issue) => {
+		const $container = $('<li>').addClass('card__container')
+		const $cardTitle = $('<h3>').text(issue.title);
+		const $cardByline = $('<h4>').text(issue.user.login);
+		const $cardLink = $(`<a href=${ issue.html_url} target="_blank">Repo Link</a>`);
+		
+		const $labels = $(`<p>`).text(issue.labels);
+		
+		this.issue.labels.forEach((label) => {
+			console.log(label.name)
+		});
+		// label container work 
+		// const $cardLabelContainer = $('<div>').addClass('label__container')
+		// issue.labels.forEach( function(label) {
+		// 	console.log()
+		// 	const $label = $('<p>').text(label.name);
+		// 	$cardLabelContainer.append($label)
+		// 	$('.card__container').append($cardLabelContainer)
+		// });
+
+
+		$container.append($cardTitle,$cardByline,$cardLink,$labels);
+		$('.gallery').append($container);
+	});
+}
+
+thisApp.getIssues = () => {
 	console.log('Ajax')
-	$.ajax({
+	return $.ajax({
 		url: thisApp.endpoint,
 		method: 'GET',
 		dataType:'json',
 		data: {
 			// user-agent: 'jamielockie'
 			token: thisApp.token,
-			q: `is:open label:"help wanted" label:"beginner" language:css` 
+			q: `is:public label:${thisApp.label__primary} label:"help wanted" language:${thisApp.language}`,
+			// sort: 'stars',
 			},
 	})
 	.then(function(res) {
-		console.log(res)
+		const issues = res.items;
+		console.log(res);
+		thisApp.displayIssues(issues)
 	})
 };
-
-// var settings = {
-// 	"async": true,
-// 	"crossDomain": true,
-// 	"url": "https://api.github.com/search/repositories?q=label%3A%22help%20wanted%22&q=stars%3A15..100&is%3Apublic=",
-// 	"method": "GET",
-// 	"headers": {
-// 		"authorization": "Basic amFtaWVsb2NraWU6Smw1NjQ5Li4=",
-
-// 	}
-// }
-
-// $.ajax(settings).done(function (response) {
-// 	console.log(response);
-// });
 
 $(thisApp.init);
