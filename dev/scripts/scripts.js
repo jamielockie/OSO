268afb7e0573336de0675fa5282b1a0e8704c262
Jamie Lockie <jamielockie@gmail.com>
2017-11-02 11:03:13 -0400

    Replacing Plugin with custom ToolTip

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -15,42 +15,33 @@ thisApp.formInputs.label = 'beginner'
 thisApp.init = () => {
 	thisApp.getIssues()
 	thisApp.events()
-	thisApp.plugins()
 };
 
 thisApp.events = function() {
-
 	$('.form__container').on('submit', function(e) { 
 		e.preventDefault();
 		$('.cardsContainer').empty();
 		// Grab Input Value and put in formInputs Object
 		thisApp.formInputs.language = $(this).find('.form__input--language').val();
-		
 		thisApp.formInputs.label = $(this).find('.form__input--label').val();
 		thisApp.getIssues()
-		console.log(thisApp.formInputs)
 	});
 };
 
 thisApp.displayIssues = (issues) => {
 	issues.forEach((issue, index) => {
 		let urlStringArray = `${issue.html_url}`.split("/")
-		console.log(urlStringArray)
-
 		const labels = issue.labels
 		const $container = $('<li>').addClass('card__container');
 		const $issueName = $('<h3>').text(issue.title).addClass('card__desc');
 		const $cardByline = $(`<p>Issue opened by <a class="card__user--link" href="https://github.com/${issue.user.login}">${issue.user.login}</a></p>`).addClass('card__user');
 		const $cardLink = $(`<div class="card__linkContainer"><a class="card__link btn--crisp" href=${ issue.html_url} target="_blank">Go to Issue</a></div>`)
-
 		const $repoName = $(`<h2><span>${urlStringArray[3]}</span> / ${urlStringArray[4]}</h2>`).addClass('card__title');
-
 		$container.append($repoName,$issueName,$cardByline);
 		const $labelContainer = $('<div>').addClass('card__labelContainer');
 		const $labelTagContainer = $('<ul>').addClass('card__labelTagContainer');
 		labels.forEach((label) => {
 			const $labels = $(`<li>${label.name}</li>`).addClass('card__label');
-
 			$labelTagContainer.append($labels);
 			$labelContainer.append($cardLink, $labelTagContainer,)
 			$container.append($labelContainer);
@@ -74,20 +65,11 @@ thisApp.getIssues = () => {
 	})
 	.then(function(res) {
 		const issues = res.items;
-		console.log(issues)
 		$.when(issueCall)
 			.then(function() {
 				thisApp.displayIssues(issues);
 			});
 	})
-};
-
-thisApp.plugins = () => {
-	$('.tip').tipr({
-		'speed': 300,
-		'mode': 'above',
-		'space': 70
-   });
 };
 
 $(thisApp.init);
